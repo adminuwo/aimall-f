42db29362373b8deebfa87fe5eac51377b2801e6
Abha <devanshtantwaynir@gmail.com>
2026-03-29 13:30:29 +0530

    Update: Added site favicon

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  // Base public path when served in production
+  base: '/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        aseries: resolve(__dirname, 'a-series.html'),
+        partner: resolve(__dirname, 'partner.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        admin: resolve(__dirname, 'admin.html'),
+      },
+    },
+  },
+});
